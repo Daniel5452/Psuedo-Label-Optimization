3f197027804f61edc1f8fefd3cb287d914984645
Daniel5452 <danielosman5@gmail.com>
2025-07-11 16:20:33 +0200

    Remove scrap folder and database files from tracking

--- scrap/revert.py
+++
@@ -1,7 +0,0 @@
-from onedl.client import connect_to_project
-
-client = connect_to_project('daniel-osman---streamlining-annotation-bootstrapping/pipeline-test')
-
-past = client.datasets.load('pseudo-f0:1')
-new_version_name = client.datasets.save('pseudo-f0', past, exist="versions")
-client.datasets.push(new_version_name, push_policy="version")
